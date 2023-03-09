49c4914488007d10a7fa87a58b30ff882ddbb9f2
MauricioFernanddes <mauriciojrfs@hotmail.com>
2023-03-09 17:31:53 -0300

    configurando redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,10 @@
-// configure aqui sua store
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { applyMiddleware, legacy_createStore as creatStore } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = creatStore(rootreducer, composeWithDevTools(applyMiddleware(thunk)));
+
+if (window.Cypress) {
+  window.store = store;
+}
+export default store;
