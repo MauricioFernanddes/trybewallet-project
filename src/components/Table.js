4c98a3ec108cd3000406caa7aaf2f5da0be7b478
MauricioFernanddes <mauriciojrfs@hotmail.com>
2023-03-14 20:51:00 -0300

    alterações req 07 e iniciando o 08

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,27 +1,69 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const tableHeader = [
+      'Descrição',
+      'Tag',
+      'Método de pagamento',
+      'Valor',
+      'Moeda',
+      'Câmbio utilizado',
+      'Valor convertido',
+      'Moeda de conversão',
+      'Editar/Excluir',
+    ];
+    const { expenses } = this.props;
     return (
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Descrição</th>
-              <th>Tag</th>
-              <th>Método de pagamento</th>
-              <th>Valor</th>
-              <th>Moeda</th>
-              <th>Câmbio utilizado</th>
-              <th>Valor convertido</th>
-              <th>Moeda de conversão</th>
-              <th>Editar/Excluir</th>
+      <table>
+        <thead>
+          <tr>
+            {tableHeader.map((element) => (
+              <th key={ element }>{element}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{(Number(expense.value)).toFixed(2)}</td>
+              <td>{expense.currency}</td>
+              <td>{Number(expense.exchangeRates[expense.currency].ask).toFixed(2)}</td>
+              <td>
+                {
+                  (Number(expense.value)
+                  * Number(expense.exchangeRates[expense.currency].ask)).toFixed(2)
+                }
+              </td>
+              <td>{expense.exchangeRates[expense.currency].name}</td>
+              <td>
+                <button
+                  type="button"
+                  data-testid="delete-btn"
+                  onClick={ () => this.handleClick(expense.id) }
+                >
+                  Deletar
+                </button>
+              </td>
             </tr>
-          </thead>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
